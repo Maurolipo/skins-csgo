8464e6cbe827bcc52f65ca79c0ca7d31db92f41d
Mauro Mattio <maurosetonix@gmail.com>
2023-04-04 19:00:03 -0300

    readme

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useParams } from "react-router-dom";
 
-import { products } from "../../productsMock";
 import ItemCount from "../ItemCount/ItemCount";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import {getDoc, collection, doc} from "firebase/firestore"
+import { db } from "../../firebaseConfig";
 
 const ItemDetailContainer = () => {
   const { id } = useParams();
 
   const {agregarAlCarrito, getQuantityById} = useContext( CartContext)
 
-  const productSelected = products.find((element) => element.id === Number(id));
+  const [productSelected, setProductSelected] = useState({})
+
+  useEffect(()=>{
+      const itemsCollection = collection(db, "products")
+      const ref= doc( itemsCollection, id )
+      getDoc(ref)
+        .then( res => {
+          setProductSelected=({
+            ...res.data(),
+            id: res.id
+          })
+        })
+  },[id])
+
+  // const productSelected = products.find((element) => element.id === Number(id));
 
   const onAdd = (cantidad)=>{
 
